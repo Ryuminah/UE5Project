6efd79dba4af33447b473f5a88a200611d4ac760
Ryuminah <Ryuminah@github.com>
2025-01-09 17:11:20 +0900

    언리얼 빌드 시스템

--- UE5Part1/Source/UE5Part1/UE5Part1.Build.cs
+++ UE5Part1/Source/UE5Part1/UE5Part1.Build.cs
@@ -8,7 +8,7 @@ public class UE5Part1 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Json", "JsonUtilities", "AssetRegistry"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Json", "JsonUtilities", "AssetRegistry"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
